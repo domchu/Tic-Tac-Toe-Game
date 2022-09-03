e8f6729859885a062becc69b7ded06da884646cd
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-04 00:07:05 +0100

    chore:added the function in js

--- JAVASCRIPT/app.js
+++ JAVASCRIPT/app.js
@@ -12,3 +12,10 @@ let conditions = [
   [0, 4, 8],
   [2, 4, 6],
 ];
+const ticTacToe = (element, index) => {
+  element.value = currentPlayer;
+  element.disabled = true;
+  cells[index] = currentPlayer;
+  currentPlayer = currentPlayer == "X" ? "0" : "X";
+  result.innerHTML = `Player ${currentPlayer} Turn`;
+};
